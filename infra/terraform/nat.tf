ff4c776e89ce6bfd095d9aaf1eca3174fb4dae8d
Fellow-DevJk <vatsa_jayantk@outlook.com>
2025-10-02 22:04:24 +0000

    feat: Added a NAT gateway

---
+++ infra/terraform/nat.tf
@@ -0,0 +1,26 @@
+#This will define the Cloud Router and the Cloud NAT gateway that attaches to it.
+
+# A Cloud Router is required to manage the Cloud NAT gateway.
+resource "google_compute_router" "main" {
+  name    = "netprobe-router"
+  network = google_compute_network.main.name
+  region  = var.region
+}
+
+# The Cloud NAT gateway itself.
+resource "google_compute_router_nat" "main" {
+  name                               = "netprobe-nat-gateway"
+  router                             = google_compute_router.main.name
+  region                             = var.region
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.analysis.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
