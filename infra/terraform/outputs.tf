e42d7c3391f9adbe3f26e033757df4bd8457441e
Fellow-DevJk <vatsa_jayantk@outlook.com>
2025-10-08 13:38:17 +0000

    minor fix 15

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,5 @@
+# infra/terraform/outputs.tf (Corrected)
+
 output "netprobe_db_private_ip" {
   description = "The private IP address of the Cloud SQL instance (for the Log Shipper)."
   value       = google_sql_database_instance.netprobe_db.private_ip_address
@@ -8,13 +10,16 @@ output "db_connection_name" {
   value       = google_sql_database_instance.netprobe_db.connection_name
 }
 
+# --- CORRECTED OUTPUTS BELOW ---
+
 output "test_workload_vm_name" {
-  description = "The name of the test workload VM (for CI/CD Tests)."
-  # The [0] is needed because the resource uses the 'count' meta-argument.
-  value       = google_compute_instance.test_workload[0].name
+  description = "The name of the test workload VM (only created when nva_instance_count > 0)."
+  # This condition prevents the error when no VM is created.
+  value       = var.nva_instance_count > 0 ? google_compute_instance.test_workload[0].name : "not_created"
 }
 
 output "test_workload_vm_zone" {
-  description = "The zone of the test workload VM (for CI/CD Tests)."
-  value       = google_compute_instance.test_workload[0].zone
+  description = "The zone of the test workload VM (only created when nva_instance_count > 0)."
+  # This condition prevents the error when no VM is created.
+  value       = var.nva_instance_count > 0 ? google_compute_instance.test_workload[0].zone : "not_created"
 }
