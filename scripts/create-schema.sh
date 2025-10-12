e93519c536b737fff6611d3b83aed933fcb5ee77
Fellow-DevJk <vatsa_jayantk@outlook.com>
2025-10-12 15:40:36 +0000

    minor fix 42

--- scripts/create-schema.sh
+++ scripts/create-schema.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Read the database IP from the first command-line argument
-DB_IP=$1
+DB_IP=$(cat ./infra/terraform/db_ip.txt)
 
 echo "--- [create-schema.sh] Applying schema to database at host: $DB_IP ---"
 
@@ -25,10 +25,7 @@ if ! pg_isready -h $DB_IP -U netprobe_user -d netprobe_logs; then
   echo "Database at $DB_IP did not become ready after 5 minutes."
   exit 1
 fi
-
-# Apply the schema
-echo "--- Database is ready. Applying schema. ---"
 psql --host=$DB_IP --username=netprobe_user --dbname=netprobe_logs <<-EOF
   CREATE TABLE connections ( id SERIAL PRIMARY KEY, ts TIMESTAMP WITH TIME ZONE NOT NULL, uid VARCHAR(255) UNIQUE NOT NULL, source_ip INET NOT NULL, source_port INTEGER NOT NULL, destination_ip INET NOT NULL, destination_port INTEGER NOT NULL, proto VARCHAR(6), service VARCHAR(255), duration FLOAT, orig_bytes BIGINT, resp_bytes BIGINT, conn_state VARCHAR(10), logged_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() );
 EOF
-echo "--- Schema applied successfully to $DB_IP ---"
+echo "--- Schema applied successfully to $DB_IP ---
