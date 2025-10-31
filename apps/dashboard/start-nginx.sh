6b5d8154874a9381315a744f707ff90fe362058d
Fellow-DevJk <vatsa_jayantk@outlook.com>
2025-10-31 17:40:20 +0000

    minor fix 92

--- apps/dashboard/start-nginx.sh
+++ apps/dashboard/start-nginx.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
-# This script is run when the container starts
+set -e
 
-# Read the PORT environment variable (default to 8080)
+# Read env vars provided by Cloud Run
 PORT="${PORT:-8080}"
+API_URL="${API_SERVICE_URL:-http://api-not-set.com}"
 
-# Read the API_SERVICE_URL, default to a placeholder if not set
-# (Note: The |g acts as a delimiter for sed)
-API_URL="${API_SERVICE_URL:-http://api-not-configured}"
+# Replace the placeholders in the config file
 sed -i "s|__PORT__|$PORT|g" /etc/nginx/conf.d/default.conf
-sed -i "s|__API_SERVICE_URL__|$API_URL/|g" /etc/nginx/conf.d/default.conf
+sed -i "s|__API_SERVICE_URL__|$API_URL|g" /etc/nginx/conf.d/default.conf
 
-# Start Nginx
 echo "Starting Nginx on port $PORT..."
 echo "Proxying /api/ to $API_URL/"
 nginx -g 'daemon off;'
