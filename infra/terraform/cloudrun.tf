41582bed2ce31e78568f3877da387977f0363907
Fellow-DevJk <vatsa_jayantk@outlook.com>
2025-11-04 10:34:34 +0000

    ingress egress change

--- infra/terraform/cloudrun.tf
+++ infra/terraform/cloudrun.tf
@@ -43,20 +43,19 @@ resource "google_cloud_run_v2_service" "api" {
   project  = var.project_id
   location = var.region
   
-  # This controls who can access it.
-  ingress = "INGRESS_TRAFFIC_ALL"
-  
+  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
+
   template {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello" # Placeholder
     }
     
-    # The 'vpc_access' block correctly defines egress for VPC traffic
     vpc_access {
       connector = google_vpc_access_connector.main.id
-      # This setting routes all outbound traffic through the VPC
-      egress    = "PRIVATE_RANGES_ONLY" 
+      egress    = "PRIVATE_RANGES_ONLY"
     }
+
+    # Explicitly tell the API to run as the Compute Engine default SA
     service_account = data.google_compute_default_service_account.default.email
   }
 
