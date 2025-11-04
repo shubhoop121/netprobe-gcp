d9e3fccfd44567cb77f8b37b6e538af00fa5ab82
Fellow-DevJk <vatsa_jayantk@outlook.com>
2025-11-04 11:29:14 +0000

    minor fix 109

--- apps/dashboard/start-nginx.sh
+++ apps/dashboard/start-nginx.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
-set -e
+set -ex
+
+echo "--- [DEBUG] STARTING DASHBOARD CONTAINER ---"
 
 PORT="${PORT:-8080}"
-API_URL="${API_SERVICE_URL:-http://api-not-setcom}"
-API_URL="${API_URL%/}"
-sed -i "s|__PORT__|$PORT|g" /etc/nginx/conf.d/default.conf
-sed -i "s|__API_SERVICE_URL__|$API_URL|g" /etc/nginx/conf.d/default.conf
+API_URL="${API_SERVICE_URL:-http://api-not-configured}"
 
-echo "Starting Nginx on port $PORT, proxying /api/ to $API_URL"
+echo "--- [DEBUG] PORT = $PORT"
+echo "--- [DEBUG] API_SERVICE_URL = $API_URL"
+
+CONFIG_FILE="/etc/nginx/conf.d/default.conf"
+
+echo "--- [DEBUG] Original nginx.conf: ---"
+cat $CONFIG_FILE
+echo "----------------------------------------"
+
+sed -i "s|__PORT__|$PORT|g" $CONFIG_FILE
+sed -i "s|__API_SERVICE_URL__/|$API_URL|g" $CONFIG_FILE
+
+echo "--- [DEBUG] FINAL NGINX CONFIG (after sed): ---"
+cat $CONFIG_FILE
+echo "----------------------------------------"
+
+echo "--- [DEBUG] Starting Nginx... ---"
 nginx -g 'daemon off;'
