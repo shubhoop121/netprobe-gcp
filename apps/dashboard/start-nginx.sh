812d49349c5e1b97396e78468a86b85057086fb2
Fellow-DevJk <vatsa_jayantk@outlook.com>
2025-11-04 16:04:25 +0000

    minor fix 120

--- apps/dashboard/start-nginx.sh
+++ apps/dashboard/start-nginx.sh
@@ -4,10 +4,7 @@ set -ex
 echo "--- [DEBUG] STARTING DASHBOARD CONTAINER ---"
 
 PORT="${PORT:-8080}"
-API_URL="${API_SERVICE_URL:-http://api-not-configured}"
-
 echo "--- [DEBUG] PORT = $PORT"
-echo "--- [DEBUG] API_SERVICE_URL = $API_URL"
 
 CONFIG_FILE="/etc/nginx/conf.d/default.conf"
 
@@ -15,8 +12,8 @@ echo "--- [DEBUG] Original nginx.conf: ---"
 cat $CONFIG_FILE
 echo "----------------------------------------"
 
+# Only replace the PORT placeholder
 sed -i "s|__PORT__|$PORT|g" $CONFIG_FILE
-sed -i "s|__API_SERVICE_URL__|${API_URL}|g" $CONFIG_FILE
 
 echo "--- [DEBUG] FINAL NGINX CONFIG (after sed): ---"
 cat $CONFIG_FILE
